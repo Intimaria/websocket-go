7e8dc79db828994933bfe6c7b7c92d75a7cebc80
Inti Tidball <inti.tidball@mikroways.net>
2023-02-07 22:28:51 -0300

    fix, modularize w getUser function

--- main.go
+++ main.go
@@ -11,17 +11,21 @@ type User struct {
 	Name string
 }
 
+func getUser(name string) User {
+	return User{Name: name}
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	t := template.New("index.html")
 	t, _ = t.ParseFiles("index.html")
-	s := User{Name: "Inti"}
+	s := getUser("Inti")
 	t.Execute(w, s)
 }
 
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 	t := template.New("hi")
 	t, _ = t.Parse("Hi, {{ .Name }}!")
-	s := User{Name: "Inti"}
+	s := getUser("you")
 	t.Execute(w, s)
 }
 
